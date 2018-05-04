eedb5db90347a746e6d03fef710484c33206f584
Jose A. Jimenez-Berni <berni@ias.csic.es>
2018-05-04 13:23:09 +0200

    Optional Wifi

--- boot.py
+++ boot.py
@@ -3,7 +3,7 @@ import machine
 from network import WLAN
 import os
 import time
-from config import SSID, PSK
+from config import SSID, PSK, WIFI_ENABLED
 
 uart = UART(0, 115200)
 os.dupterm(uart)
@@ -11,18 +11,19 @@ os.dupterm(uart)
 rtc = machine.RTC()
 
 wlan = WLAN() # get current object, without changing the mode
-#wlan.deinit()
+if WIFI_ENABLED:
+    if machine.reset_cause() != machine.SOFT_RESET:
+        wlan.init(mode=WLAN.STA)
+        # configuration below MUST match your home router settings!!
+        #wlan.ifconfig(config=('192.168.1.101', '255.255.255.0', '192.168.1.1', '8.8.8.8'))
 
-if machine.reset_cause() != machine.SOFT_RESET:
-    wlan.init(mode=WLAN.STA)
-    # configuration below MUST match your home router settings!!
-    #wlan.ifconfig(config=('192.168.1.101', '255.255.255.0', '192.168.1.1', '8.8.8.8'))
-
-if not wlan.isconnected():
-    # change the line below to match your network ssid, security and password
-    wlan.connect(SSID, auth=(WLAN.WPA2, PSK), timeout=5000)
-    while not wlan.isconnected():
-        machine.idle() # save power while waiting
-    print('WLAN connection succeeded!')
-    print(wlan.ifconfig())
-    rtc.ntp_sync("pool.ntp.org")
+    if not wlan.isconnected():
+        # change the line below to match your network ssid, security and password
+        wlan.connect(SSID, auth=(WLAN.WPA2, PSK), timeout=5000)
+        while not wlan.isconnected():
+            machine.idle() # save power while waiting
+        print('WLAN connection succeeded!')
+        print(wlan.ifconfig())
+        rtc.ntp_sync("pool.ntp.org")
+else:
+    wlan.deinit()
